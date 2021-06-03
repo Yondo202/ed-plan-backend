008055d5aad39d45cb6e99e16c38a0e9614696c8
Yondo202 <yondooo61@gmail.com>
2021-06-03 18:16:51 +0800

    file uploads add

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // url: '/api', 
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0a4735821b1879f6ab1fa193f5497187'),
-    },
+    }
   },
 });
