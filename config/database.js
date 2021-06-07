bbf7ccf7792fa1f0b2b361c556e904510fb2c809
Yondo202 <yondooo61@gmail.com>
2021-06-07 11:11:27 +0800

    dd

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.rsabc.mongodb.net'),
+        host: env('DATABASE_HOST', 'ed-plan.zxhth.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'ed-plan-back'),
-        username: env('DATABASE_USERNAME', 'edp'),
+        database: env('DATABASE_NAME', 'ed-plan'),
+        username: env('DATABASE_USERNAME', 'ed-plan'),
         password: env('DATABASE_PASSWORD', 'killer951988'),
       },
       options: {
