80e9383c5178152afc829cca76eaea99dc8890de
Yondo202 <yondooo61@gmail.com>
2021-08-25 18:23:15 +0800

    dd

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: 'https://pimis-plan.edp.mn/pimis-edplan',
+  url: 'https://pimis-plan.edp.mn/pimis-edplan',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '0a4735821b1879f6ab1fa193f5497187'),
