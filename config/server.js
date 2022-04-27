e349945a33a260b62871c60b1da79dc10e498418
Yondo202 <yondooo61@gmail.com>
2022-04-27 09:25:58 +0800

    dd

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: 'https://pimis-plan.edp.mn/pimis-edplan',
+  url: 'https://pimis-plan.edp.mn/pimis-edplan',
   // url: 'http://192.168.88.232:1337/pimis-edplan',
   admin: {
     auth: {
